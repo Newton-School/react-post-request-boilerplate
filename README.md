942370c0bbe0df8b27e5a0e29a5a64ed97e2fb14
Shardul Kurdukar <24shardul@gmail.com>
2022-03-28 18:22:27 +0530

    changes done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# boilerplate-react-functional-public
+POST Request
+1. Send a fetch request to "/api" url with POST method.
+2. You will get a code(a string) as a response in the response body.
+3. Set the postCode state to the code you received as a response. 
+Hint: Call the fetch method inside the useEffect() function.
--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,20 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
-import '../styles/App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "../styles/App.css";
 const App = () => {
   const [postCode, setpostCode] = useState(null);
   useEffect(() => {
-    const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title: 'React Hooks POST Request Example' })
-  };
-  fetch('/api', requestOptions)
-      .then(response => response.json())
-      .then(data => setpostCode(data.code));
-  }, [])
+
+  }, []);
 
   return (
     <div id="main">
-      <h1>Display the code you receive on doing a POST request in the given div</h1>
+      <h1>
+        Display the code you receive on doing a POST request in the given div
+      </h1>
       <h2 id="postCode">{postCode}</h2>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
